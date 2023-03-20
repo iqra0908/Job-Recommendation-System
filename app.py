f990bea7cf8db87ea4ae642e009784068f27f314
Iqra Imtiaz <iqra_imtiaz09@hotmail.com>
2023-03-20 17:29:42 -0400

    adding python app for job recommendation system

---
+++ app.py
@@ -0,0 +1,27 @@
+from scripts.job_matching import JobMatching
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+job = JobMatching()
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/jobs', methods=['GET'])
+def getJobs():
+    return job.get_all_jobs()
+
+@app.route('/jobsByResume', methods=['POST'])
+def getJobsMatchedWithResume():
+    if 'resume' in request.data.decode():
+        resume = request.data.decode()
+    else:
+        # If no resume data is found, return an error message
+        return "Error: No resume data provided."
+
+    # Pass the resume data to the get_jobs_matched() function
+    return job.get_jobs_matched(resume)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
---
+++ scripts/job_matching.py
@@ -0,0 +1,76 @@
+import spacy
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+#sys.path.append('../')
+
+class JobMatching:
+    def __init__(self):
+        self.jobs = self.load_jobs().head(500)
+    
+    def load_resume(self):
+        resume = open("data/Resume-Iqra-2023.txt").read()
+        return resume
+    
+    def load_jobs(self):
+        jobs = pd.read_csv("data/job_posts.csv")
+        jobs.dropna(subset=['JobDescription','JobRequirment'], inplace=True)
+        jobs.fillna('', inplace=True)
+        return jobs
+    
+    def get_all_jobs(self):
+        jobs = self.jobs
+        jobs = jobs[['date','Title','Company','Eligibility','Location','JobDescription','JobRequirment','RequiredQual','ApplicationP']]
+        return jobs.head(20).to_dict(orient='records')
+    
+    def keyword_matching(self,resume,jobs):
+        vectorizer = CountVectorizer()
+        keywords = vectorizer.fit_transform([resume]).toarray()[0]
+
+        keywords_match = []
+        for i in range(len(jobs)):
+            job_description_keywords = vectorizer.transform([jobs['JobDescription'].iloc[i]]).toarray()[0]
+            job_requirement_keywords = vectorizer.transform([jobs['JobRequirment'].iloc[i]]).toarray()[0]
+            match = all(job_description_keywords[i] >= keywords[i] or job_requirement_keywords[i] >= keywords[i] for i in range(len(keywords)))
+            keywords_match.append(match)
+
+        # Add keywords_match column to books dataframe
+        jobs['keywords_match'] = keywords_match
+        
+    def cosine_similarity(self,resume,jobs):
+        vectorizer = TfidfVectorizer()
+        resume_vector = vectorizer.fit_transform([resume])
+
+        desc_similarity_scores = []
+        req_similarity_scores = []
+        for i in range(len(jobs)):
+            job_description_vector = vectorizer.transform([jobs['JobDescription'].iloc[i]])
+            job_requirement_vector = vectorizer.transform([jobs['JobRequirment'].iloc[i]])
+            desc_similarity = cosine_similarity(resume_vector, job_description_vector)[0][0]
+            req_similarity = cosine_similarity(resume_vector, job_requirement_vector)[0][0]
+            desc_similarity_scores.append(desc_similarity)
+            req_similarity_scores.append(req_similarity)
+            
+        jobs['desc_similarity'] = desc_similarity_scores
+        jobs['req_similarity'] = req_similarity_scores
+        
+    def get_jobs_matched(self,resume):
+        jobs = self.jobs
+        self.cosine_similarity(resume,jobs)
+        jobs = jobs.sort_values(by=['desc_similarity','req_similarity'], ascending=False)
+        jobs = jobs[['date','Title','Company','Eligibility','Location','JobDescription','JobRequirment','RequiredQual','ApplicationP']]
+        return jobs.head(20).to_dict(orient='records')
+        
+if __name__ == '__main__':
+    job_matching = JobMatching()
+    resume = job_matching.load_resume()
+    jobs = job_matching.load_jobs()
+    #job_matching.keyword_matching(resume,jobs)
+    job_matching.cosine_similarity(resume,jobs)
+    print(jobs[jobs['desc_similarity'] > 0.5])
+    #print(jobs[jobs['keywords_match']])
+    
+
+        
