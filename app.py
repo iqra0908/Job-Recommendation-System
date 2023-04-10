382e5ee1e257b96e451c180434dbaa85f3816f92
Iqra Imtiaz <iqra_imtiaz09@hotmail.com>
2023-04-09 23:43:40 -0400

    adding azure functions

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from scripts.job_matching import JobMatching
 from flask import Flask, request, render_template
+import azure.functions as func
 
 app = Flask(__name__)
 job = JobMatching()
+
+def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    """Each request is redirected to the WSGI handler.
+    """
+    return func.WsgiMiddleware(app.wsgi_app).handle(req, context)
 
 @app.route("/")
 def index():
--- scripts/job_matching.py
+++ scripts/job_matching.py
@@ -3,6 +3,14 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from gensim.models import Word2Vec
+from gensim.models.fasttext import FastText
+from keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from keras.layers import Input, Embedding, LSTM, Dense, Lambda
+from keras.models import Model
+from keras import backend as K
+import gensim.downloader as api
 
 class JobMatching:
     def __init__(self):
@@ -45,6 +53,80 @@ class JobMatching:
         # Add keywords_match column to books dataframe
         jobs['keywords_match'] = keywords_match
         
+
+    def siamese_similarity(self, text1, text2):
+        # Tokenize the texts
+        tokenizer = Tokenizer()
+        tokenizer.fit_on_texts([text1, text2])
+
+        # Convert the texts to sequences of indices
+        seq1 = tokenizer.texts_to_sequences([text1])[0]
+        seq2 = tokenizer.texts_to_sequences([text2])[0]
+
+        # Pad the sequences to a fixed length
+        max_len = 20
+        padded_seq1 = pad_sequences([seq1], maxlen=max_len, padding='post', truncating='post')[0]
+        padded_seq2 = pad_sequences([seq2], maxlen=max_len, padding='post', truncating='post')[0]
+
+        # Load the pre-trained FastText model
+        #model = FastText.load_fasttext_format('cc.en.300.bin')
+        model = api.load('fasttext-wiki-news-subwords-300')
+
+        # Encode the sequences using the FastText model
+        embedding_matrix = np.zeros((len(tokenizer.word_index) + 1, model.vector_size))
+        for word, i in tokenizer.word_index.items():
+            if word in model:
+                embedding_matrix[i] = model[word]
+
+        left_input = Input(shape=(max_len,))
+        right_input = Input(shape=(max_len,))
+
+        embedding_layer = Embedding(input_dim=len(tokenizer.word_index) + 1,
+                                    output_dim=model.vector_size,
+                                    weights=[embedding_matrix],
+                                    input_length=max_len,
+                                    trainable=False)
+
+        left_embedded = embedding_layer(left_input)
+        right_embedded = embedding_layer(right_input)
+
+        lstm_units = 64
+        lstm_layer = LSTM(lstm_units)
+
+        left_lstm = lstm_layer(left_embedded)
+        right_lstm = lstm_layer(right_embedded)
+
+        # Define the similarity function (in this case, cosine similarity)
+        def cosine_similarity(x):
+            left, right = x
+            dot_product = left * right
+            dot_product = K.sum(dot_product, axis=1, keepdims=True)
+            left_norm = K.sqrt(K.sum(K.square(left), axis=1, keepdims=True))
+            right_norm = K.sqrt(K.sum(K.square(right), axis=1, keepdims=True))
+            return dot_product / (left_norm * right_norm)
+
+        similarity_layer = Lambda(cosine_similarity)
+
+        similarity = similarity_layer([left_lstm, right_lstm])
+
+        siamese_model = Model(inputs=[left_input, right_input], outputs=similarity)
+
+        siamese_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+        # Predict the similarity between the two texts
+        similarity_score = siamese_model.predict([padded_seq1.reshape(1, -1), padded_seq2.reshape(1, -1)])[0][0]
+        return similarity_score
+
+    
+    def siamese_scores(self,resume,jobs):
+        similarity_scores = []
+        for i in range(len(jobs)):
+            similarity = self.siamese_similarity(resume, jobs['JobDescription'].iloc[i])
+            similarity_scores.append(similarity)
+            
+        jobs['siamese_similarity'] = similarity_scores
+        jobs = jobs.sort_values('siamese_similarity', ascending=False)
+            
     def cosine_similarity(self,resume,jobs):
         vectorizer = TfidfVectorizer()
         resume_vector = vectorizer.fit_transform([resume])
@@ -60,6 +142,7 @@ class JobMatching:
     def get_jobs_matched(self,resume):
         jobs = self.jobs
         self.cosine_similarity(resume,jobs)
+        self.siamese_scores(resume,jobs)
         jobs = jobs.sort_values(by=['cosine_similarity'], ascending=False)
         #jobs = jobs[['date','Title','Company','Eligibility','Location','JobDescription','JobRequirment','RequiredQual','ApplicationP']]
         return jobs.head(20).to_dict(orient='records')
