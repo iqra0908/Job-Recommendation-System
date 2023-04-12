2ace1eab5331f60ba12bdb137aa85267701a8074
Iqra Imtiaz <iqra_imtiaz09@hotmail.com>
2023-04-12 00:16:10 -0400

    removing unnecessary files

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-mod job_matching;
-
-fn main() {
-    let mut job_matching = job_matching::JobMatching::new();
-    let resume = job_matching.load_resume();
-    let results = job_matching.get_jobs_matched(&resume);
-    println!("{:?}", results);
-}
